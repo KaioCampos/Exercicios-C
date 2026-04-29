055a73b5d98cdf77f2e2ac16e46d9796022b0dcb
KaioCampos <kaiocampos373@gmail.com>
2026-04-28 21:08:45 -0300

    Add header file for Hangman game functions

---
+++ exercicio3/forca.h
@@ -0,0 +1,17 @@
+#ifndef FORCA_H
+#define FORCA_H
+
+#define TAMANHO_PALAVRA 20
+
+int letraexiste(char letra);
+int chuteserrados();
+int enforcou();
+int ganhou();
+void abertura();
+void chuta();
+int jachutou(char letra);
+void desenhaforca();
+void escolhepalavra();
+void adicionapalavra();
+
+#endif
